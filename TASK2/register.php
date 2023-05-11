7b333936108de10f1755d6786a499e3b664b8d9c
SAKIRSAIFI <111906150+SAKIRSAIFI@users.noreply.github.com>
2023-05-11 11:27:47 +0530

    Update register.php

--- TASK2/register.php
+++ TASK2/register.php
@@ -1,7 +1,6 @@
 <?php
 include_once("connection.php");
 
-$result=mysqli_query($con,$sql);
 $username = $_POST["username"];
 $email = $_POST["email"];
 $password = $_POST["password"];
@@ -9,6 +8,7 @@ $confirm_password = $_POST["confirm-password"];
 
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $sql = "INSERT INTO login (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+$result=mysqli_query($con,$sql);
 	if ($con->query($sql) === TRUE) {
 		echo "User registered successfully!";
 	} else {
